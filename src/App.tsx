6ec2402f9e98c17a518e484bdc55497e506003dd
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-01-10 15:37:10 +0000

    Added tags state and useEffect to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Home from "./routes/Home";
 import IndividualResource from "./routes/IndividualResource";
 import HomeLoggedIn from "./routes/HomeLoggedIn";
 import AddResource from "./routes/AddResource";
 import { IUser } from "./interfaces/IUser";
 import { IResourceShort } from "./interfaces/IResource";
+import { ITag } from "./interfaces/ITag";
+import { fetchData } from "./utils/fetchData";
+import { baseUrl } from "./utils/baseUrl";
 
 function App(): JSX.Element {
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const [allResources, setAllResources] = useState<IResourceShort[]>([]);
   const [allUsers, setAllUsers] = useState<IUser[]>([]);
   const [user, setUser] = useState<IUser | undefined>();
+  const [tags, setTags] = useState<ITag[]>([]);
+  useEffect(() => {
+    fetchData(baseUrl + "/resources", setAllResources);
+    fetchData(baseUrl + "/users", setAllUsers);
+    fetchData(baseUrl + "/tags", setTags);
+  }, []);
   return (
     <div>
       <Router>
