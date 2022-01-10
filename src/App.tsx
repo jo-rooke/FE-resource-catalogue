49e50933fd46919634be4bf159843ac8e2829a7d
Chumphot Ngammekchay <n.chumphot@gmail.com>
2022-01-10 15:09:47 +0000

    Added routes and states

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import { greet } from "./utils/greet";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useState } from "react";
+import Home from "./routes/Home";
+import IndividualResource from "./routes/IndividualResource";
+import HomeLoggedIn from "./routes/HomeLoggedIn";
+import AddResource from "./routes/AddResource";
+import { IUser } from "./interfaces/IUser";
+import { IResourceShort } from "./interfaces/IResource";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [allResources, setAllResources] = useState<IResourceShort[]>([]);
+  const [allUsers, setAllUsers] = useState<IUser[]>([]);
+  const [user, setUser] = useState<IUser | undefined>();
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home {...{ loggedIn, setLoggedIn, user, setUser, allUsers }} />
+            }
+          />
+          <Route path="/dashboard" element={<HomeLoggedIn />} />
+          <Route path="/resource/:id" element={<IndividualResource />} />
+          <Route path="/resource/add" element={<AddResource />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
