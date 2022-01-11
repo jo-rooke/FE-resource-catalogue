98f474aaefdcc7374827eeea2a8a5f7c67d31840
Faith <faithodularu@hotmail.com>
2022-01-11 10:25:56 +0000

    fixed errors in fetchData.tsx

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,4 +1,9 @@
-export function fetchData(url: string, setState: React.SetStateAction<any>) {
+import { IResourceShort } from "../interfaces/IResource";
+import { ITag } from "../interfaces/ITag";
+import { IUser } from "../interfaces/IUser";
+
+
+export function fetchData(url: string, setState: React.Dispatch<React.SetStateAction<IResourceShort[]>> | React.Dispatch<React.SetStateAction< IUser[]>> | React.Dispatch<React.SetStateAction< ITag[]>>): void {
   fetch(url)
     .then((res) => res.json())
     .then((jsonBody) => setState(jsonBody.data));
