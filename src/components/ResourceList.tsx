c57b48d0f4618f7d5324372838c35d9e0fac4ac7
Jo <joely.rooke@gmail.com>
2022-01-12 15:58:53 +0000

    cleaned up filtering of the resources into utils

--- src/components/ResourceList.tsx
+++ src/components/ResourceList.tsx
@@ -2,8 +2,8 @@ import { IResourceShort } from "../interfaces/IResource";
 import { ITag } from "../interfaces/ITag";
 import { useState } from "react";
 import Resource from "./Resource";
-import filterTags from "../utils/filterTags";
-import filterSearch from "../utils/filterSearch";
+import filterSearchAndTags from "../utils/filterSearchAndTags";
+import filterOutStudyList from "../utils/filterOutStudyList";
 import { IUser } from "../interfaces/IUser";
 
 export default function ResourceList(props: {
@@ -16,47 +16,6 @@ export default function ResourceList(props: {
 }): JSX.Element {
   const [search, setSearch] = useState("");
   const [tagsSelected, setTagsSelected] = useState<ITag[]>([]);
-
-  let filteredResources: IResourceShort[] = props.allResources;
-
-  //filtering out resources that are already in the study list
-  let arrayToFilter;
-  if (props.studyList.length > 0) {
-    arrayToFilter = props.allResources.filter((singleResource) => {
-      for (const listItem of props.studyList) {
-        console.log(listItem.id, "list item id");
-        console.log(singleResource.id, "single resource id");
-        if (listItem.id === singleResource.id) {
-          return false;
-        }
-      }
-      return true;
-    });
-  } else {
-    arrayToFilter = props.allResources;
-  }
-
-  if (tagsSelected.length !== 0 && search.length === 0) {
-    //filtering for tags only
-    filteredResources = arrayToFilter.filter((singleResource) =>
-      filterTags(tagsSelected, singleResource)
-    );
-  } else if (tagsSelected.length === 0 && search.length !== 0) {
-    //filtering for search term only
-    filteredResources = arrayToFilter.filter((resource) =>
-      filterSearch(search, resource)
-    );
-  } else if (tagsSelected.length !== 0 && search.length !== 0) {
-    //filtering for both tags and search term
-    const filteredTags = arrayToFilter.filter((resource) =>
-      filterTags(tagsSelected, resource)
-    );
-    filteredResources = filteredTags.filter((resource) =>
-      filterSearch(search, resource)
-    );
-  } else {
-    filteredResources = arrayToFilter;
-  }
 
   const handleTagClick = (tag: ITag) => {
     let newTags: ITag[];
@@ -85,16 +44,19 @@ export default function ResourceList(props: {
         onChange={(e) => setSearch(e.target.value)}
       ></input>
       {props.user && <button>Add a new resource</button>}
-      {filteredResources.map((resource) => (
-        <Resource
-          key={resource.id}
-          resource={resource}
-          user={props.user}
-          setAllResources={props.setAllResources}
-          studyList={props.studyList}
-          setStudyList={props.setStudyList}
-        />
-      ))}
+      {props.allResources
+        .filter((item) => filterOutStudyList(item, props.studyList))
+        .filter((item) => filterSearchAndTags(item, tagsSelected, search))
+        .map((resource) => (
+          <Resource
+            key={resource.id}
+            resource={resource}
+            user={props.user}
+            setAllResources={props.setAllResources}
+            studyList={props.studyList}
+            setStudyList={props.setStudyList}
+          />
+        ))}
     </>
   );
 }
