acd9271ae9eafe6bd3c440f68142ec0dd884edec
Faith <faithodularu@hotmail.com>
2022-01-13 17:29:23 +0000

    passed down allUsers, setUser, setAllResources to AddResource

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { IResourceShort } from "./interfaces/IResource";
 import { ITag } from "./interfaces/ITag";
 import { fetchData } from "./utils/fetchData";
 import { baseUrl } from "./utils/baseUrl";
+import PageHeader from "./components/PageHeader";
 
 function App(): JSX.Element {
   const [allResources, setAllResources] = useState<IResourceShort[]>([]);
@@ -61,7 +62,7 @@ function App(): JSX.Element {
               />
             }
           />
-          <Route path="/resources/add" element={<AddResource user={user}/>} />
+          <Route path="/resources/add" element={user? <AddResource tags={tags} user={user} allUsers={allUsers} setAllResources={setAllResources} setUser={setUser}/> : <PageHeader title={"Add a Resource"} allUsers={allUsers} user={user} setUser={setUser}/>} />
         </Routes>
       </Router>
     </div>
