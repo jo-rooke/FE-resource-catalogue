351b61804d5ee771c773e1fc1394376e8bc21121
Faith <faithodularu@hotmail.com>
2022-01-18 10:36:18 +0000

    formatted

--- src/tests/doesUrlExist.test.ts
+++ src/tests/doesUrlExist.test.ts
@@ -1,8 +1,54 @@
-import doesUrlExist from '../utils/doesUrlExist'
+import doesUrlExist from "../utils/doesUrlExist";
 
 test("function to find out if the url being posted already exists", () => {
-
-    const resourceAdded1 = {
+  const resourceAdded1 = {
+    resource_name: "JavaScript tutorial",
+    author_name: "CodeCademy",
+    tags: [
+      { id: 1, name: "HTML" },
+      { id: 4, name: "JavaScript" },
+    ],
+    description: "a tutorial covering all the basics of js",
+    url: "https://www.codecademy.com/learn/introduction-to-javascript",
+    creation_date: "2022-01-07T11:19:55.556Z",
+    content_type: "video",
+    week_no: 5,
+    recommender_id: 4,
+    rec_status: "I recommend this resource after having used it",
+    rec_message: "great tutorial",
+  };
+  const resourceAdded2 = {
+    resource_name: "JavaScript tutorial",
+    author_name: "CodeCademy",
+    tags: [
+      { id: 1, name: "HTML" },
+      { id: 4, name: "JavaScript" },
+    ],
+    description: "a tutorial covering all the basics of js",
+    url: "www.codecademy.com/learn/introduction-to-javascript",
+    creation_date: "2022-01-07T11:19:55.556Z",
+    content_type: "video",
+    week_no: 5,
+    recommender_id: 4,
+    rec_status: "I recommend this resource after having used it",
+    rec_message: "great tutorial",
+  };
+  const resourceAdded3 = {
+    resource_name: "Typescript tutorial",
+    author_name: "CodeCademy",
+    tags: [{ id: 6, name: "TypeScript" }],
+    description: "a tutorial covering all the basics of ts",
+    url: "https://www.codecademy.com/learn/introduction-to-typescript",
+    creation_date: "2022-01-07T11:19:55.556Z",
+    content_type: "video",
+    week_no: 5,
+    recommender_id: 4,
+    rec_status: "I recommend this resource after having used it",
+    rec_message: "good tutorial",
+  };
+  const allResources = [
+    {
+      id: 1,
       resource_name: "JavaScript tutorial",
       author_name: "CodeCademy",
       tags: [
@@ -10,72 +56,24 @@ test("function to find out if the url being posted already exists", () => {
         { id: 4, name: "JavaScript" },
       ],
       description: "a tutorial covering all the basics of js",
-      url: "https://www.codecademy.com/learn/introduction-to-javascript",
+      url: "www.codewars.com",
       creation_date: "2022-01-07T11:19:55.556Z",
-      content_type: "video",
-      week_no: 5,
-      recommender_id:4,
-      rec_status: "I recommend this resource after having used it",
-      rec_message: "great tutorial"
-    };
-    const resourceAdded2 = {
-        resource_name: "JavaScript tutorial",
-        author_name: "CodeCademy",
-        tags: [
-          { id: 1, name: "HTML" },
-          { id: 4, name: "JavaScript" },
-        ],
-        description: "a tutorial covering all the basics of js",
-        url: "www.codecademy.com/learn/introduction-to-javascript",
-        creation_date: "2022-01-07T11:19:55.556Z",
-        content_type: "video",
-        week_no: 5,
-        recommender_id:4,
-        rec_status: "I recommend this resource after having used it",
-        rec_message: "great tutorial"
-      };
-    const resourceAdded3 = {
-        resource_name: "Typescript tutorial",
-        author_name: "CodeCademy",
-        tags: [
-          { id: 6, name: "TypeScript" },
-        ],
-        description: "a tutorial covering all the basics of ts",
-        url: "https://www.codecademy.com/learn/introduction-to-typescript",
-        creation_date: "2022-01-07T11:19:55.556Z",
-        content_type: "video",
-        week_no: 5,
-        recommender_id:4,
-        rec_status: "I recommend this resource after having used it",
-        rec_message: "good tutorial"
-      };
-    const allResources = [{
-        id: 1,
-        resource_name: "JavaScript tutorial",
-        author_name: "CodeCademy",
-        tags: [
-          { id: 1, name: "HTML" },
-          { id: 4, name: "JavaScript" },
-        ],
-        description: "a tutorial covering all the basics of js",
-        url: "www.codewars.com",
-        creation_date: "2022-01-07T11:19:55.556Z"
-      }, 
-      {
-        id: 2,
-        resource_name: "JavaScript tutorial",
-        author_name: "CodeCademy",
-        tags: [
-          { id: 1, name: "HTML" },
-          { id: 4, name: "JavaScript" },
-        ],
-        description: "a tutorial covering all the basics of js",
-        url: "codecademy.com/learn/introduction-to-javascript",
-        creation_date: "2022-01-07T11:19:55.556Z"
-    }]
-    expect(doesUrlExist(allResources, resourceAdded1)).toBeDefined();
-    expect(doesUrlExist(allResources, resourceAdded1)).toStrictEqual(true); 
-    expect(doesUrlExist(allResources, resourceAdded2)).toStrictEqual(true); 
-    expect(doesUrlExist(allResources, resourceAdded3)).toStrictEqual(false);
-  });
-  
+    },
+    {
+      id: 2,
+      resource_name: "JavaScript tutorial",
+      author_name: "CodeCademy",
+      tags: [
+        { id: 1, name: "HTML" },
+        { id: 4, name: "JavaScript" },
+      ],
+      description: "a tutorial covering all the basics of js",
+      url: "codecademy.com/learn/introduction-to-javascript",
+      creation_date: "2022-01-07T11:19:55.556Z",
+    },
+  ];
+  expect(doesUrlExist(allResources, resourceAdded1)).toBeDefined();
+  expect(doesUrlExist(allResources, resourceAdded1)).toStrictEqual(true);
+  expect(doesUrlExist(allResources, resourceAdded2)).toStrictEqual(true);
+  expect(doesUrlExist(allResources, resourceAdded3)).toStrictEqual(false);
+});
--- src/utils/doesUrlExist.ts
+++ src/utils/doesUrlExist.ts
@@ -6,8 +6,12 @@ export default function doesUrlExist(
   addResource: IResourceAdd
 ): boolean {
   for (const resource of allResources) {
-    const existingUrl = resource.url?.replace(/http[s]?:\/\//, "").replace("www.", "");
-    const newUrl = addResource.url.replace(/http[s]?:\/\//, "").replace("www.", "");
+    const existingUrl = resource.url
+      ?.replace(/http[s]?:\/\//, "")
+      .replace("www.", "");
+    const newUrl = addResource.url
+      .replace(/http[s]?:\/\//, "")
+      .replace("www.", "");
     if (existingUrl === newUrl) {
       return true;
     }
